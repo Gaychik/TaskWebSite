a8d791ac45c3d8ea7631167a5ac6fa0f438316e0
Gaychik <gaychikov2013@gmail.com>
2019-03-17 14:42:01 +0300

    FascinatingTask

---
+++ WebSite2/App_Code/Coordinate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+public class Coordinate
+{
+    [Required(AllowEmptyStrings = false, ErrorMessage = "The fields are not assign values!")]
+    [Range(0,10)]
+    public int X { get; set; }
+					
+    [Required(AllowEmptyStrings =false,ErrorMessage = "The fields are not assign values!")]
+    [Range(0,10)]
+    public int Y { get; set; }
+}
---
+++ WebSite2/App_Code/Matrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Summary description for Matrix
+/// </summary>
+public class Matrix
+{
+    public Matrix(out int[,] mas,int row,int col)
+    {
+        mas = new int[row, col];
+        for (int i = 0; i < mas.GetLength(0); i++)
+            for (int j = 0; j < mas.GetLength(1); j++)
+                mas[i, j] = -1;
+
+
+    }
+}
